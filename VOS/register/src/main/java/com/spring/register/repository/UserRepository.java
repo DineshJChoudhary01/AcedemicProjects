fc7a216e4898a1ed91b485601032a982fe059ca1
Dinesh <dineshjchoudhary01@gmail.com>
2023-03-31 09:38:27 +0530

    CDAC Project- Online Study Web Portal

--- VOS/register/src/main/java/com/spring/register/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.register.repository;
-
-import com.spring.register.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
-}
